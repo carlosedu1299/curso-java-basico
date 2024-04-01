51b8294ca5d9270033bb0d0522fc3430f3023956
Carlos Santos <cesantos1903@gmail.com>
2024-04-01 19:17:22 -0300

    COMITANDO OS CODIGOS SOBRE VARIAVEIS

--- TIPOS-VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //um numero short nao pode receber um int p pde passar de 32mil num ue comporta
+        
     }
 }
