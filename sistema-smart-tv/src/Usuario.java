8bf4e3c19246b1714c382419b6f401545a6b3945
Carlos Santos <cesantos1903@gmail.com>
2024-04-04 23:21:01 -0300

    códigos do projeto de uma simulação de tv smart

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,24 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+
+        smartTv.mudarCanal(13);
+        System.out.println("canal atual: " + smartTv.canal);
+
+        System.out.println("volume atual: " + smartTv.volume);
+
+        System.out.println("Tv ligada? " + smartTv.ligada);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV ligada? " + smartTv.ligada);
+        System.out.println("canal atual: " + smartTv.canal);
+        smartTv.desligar();
+        System.out.println("Novo Status -> Tv Ligada ? " + smartTv.ligada);
+
+    }
+}
