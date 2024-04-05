3e935b04a8f6d02ccf4fd8ae10cad80c82bebc5f
Carlos Santos <cesantos1903@gmail.com>
2024-04-04 23:40:56 -0300

    projeto da tv smart

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,8 +1,9 @@
 public class SmartTv {
+    
     boolean ligada = false;
     int canal = 1;
     int volume = 25;
-    
+
     public void mudarCanal(int novoCanal){
         canal = novoCanal;
     }
