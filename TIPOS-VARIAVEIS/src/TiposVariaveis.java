39ecd48cbb9d4bbfe5ed4a0b8bb94628b6b90b0c
Carlos Santos <cesantos1903@gmail.com>
2024-09-21 18:12:19 -0300

    começando estudo sobre variaveis

--- TIPOS-VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -5,6 +5,8 @@ public class TiposVariaveis {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal; //um numero short nao pode receber um int p pde passar de 32mil num ue comporta
-        
+        final double PI= 3.14;
+        PI=10;
+        System.out.println(PI);
     }
 }
